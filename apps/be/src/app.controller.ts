b56b878d13b58b6e7f87efb02b2578620a33abb8
tmatsiievskyi1 <matsiievskyi.taras@gmail.com>
2024-12-10 23:51:46 +0200

    swagger init

--- apps/be/src/app.controller.ts
+++ apps/be/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+
+class InitDto {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: InitDto,
+  })
+  getHello(): InitDto {
+    return { message: this.appService.getHello() };
   }
 }
